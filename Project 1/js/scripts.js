152e3adbbdb31f965181a0b5f1a11dd6bc8221ce
unknown <giuseppeacannizzojr@gmail.com>
2019-03-14 20:05:05 -0500

    Update

---
+++ Project 1/js/scripts.js
@@ -0,0 +1,9 @@
+var greeting = 'Welcome To The Jungle';
+var user = "Giuseppe!!!";
+var message = greeting + " " + user;
+
+
+var price = 400;
+var studentdiscount = .15;
+var studentprice = price - (price * studentdiscount)
+var message2 = studentprice;
