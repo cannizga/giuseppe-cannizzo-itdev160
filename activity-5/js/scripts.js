af2036b94f332a14672d22092e1d009707bc40e9
unknown <giuseppeacannizzojr@gmail.com>
2019-03-27 19:40:17 -0500

    Update

--- activity-5/js/scripts.js
+++ activity-5/js/scripts.js
@@ -1,3 +1,10 @@
-var greeting = '3 Best Atom Packages';
-var greeting2 = "That Make Atom Easier To Use";
+var greeting = "The 3 Best Packages That";
+var greeting2 = "Make Atom Easier To Use";
 var message = greeting + " " + greeting2;
+
+var information = {
+  downloads: ["810,850", "6,031,820", "87,576"],
+  stars: [673, 5054, 293],
+  description: ["A live preview tool for Atom Editor.", "Better organizes your code and makes it look more beautiful.",
+  "Quickly get answers and code samples from Stack Overflow directly in Atom editor." ],
+  author: ["harmsk", "Glavin001", "Chris911"]};
