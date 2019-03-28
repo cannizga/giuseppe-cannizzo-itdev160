ea72eb19d63aa53944546febbc5422824d261775
unknown <giuseppeacannizzojr@gmail.com>
2019-03-27 19:50:34 -0500

    Completed Activity 5

--- activity-5/js/scripts.js
+++ activity-5/js/scripts.js
@@ -3,8 +3,8 @@ var greeting2 = "Make Atom Easier To Use";
 var message = greeting + " " + greeting2;
 
 var information = {
-  downloads: ["810,850", "6,031,820", "87,576"],
-  stars: [673, 5054, 293],
+  downloads: ["810,957", "6,032,186", "87,591"],
+  stars: [673, "5,054", 293],
   description: ["A live preview tool for Atom Editor.", "Better organizes your code and makes it look more beautiful.",
   "Quickly get answers and code samples from Stack Overflow directly in Atom editor." ],
   author: ["harmsk", "Glavin001", "Chris911"]};
