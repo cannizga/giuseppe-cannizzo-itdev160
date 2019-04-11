0f3451a2e66506fd9f8aadcd3ba5d6fe184743df
unknown <giuseppeacannizzojr@gmail.com>
2019-04-10 21:45:13 -0500

    Fourth Commit

--- activity-6/js/scripts.js
+++ activity-6/js/scripts.js
@@ -1,6 +1,6 @@
 var messages = [];
 
-function messageType (name1, name2, m2, m, userInput){
+function Message (name1, name2, m2, m, userInput){
   this.name1 = name1
   this.name2 = name2;
   this.userInput = userInput;
@@ -9,8 +9,13 @@ function messageType (name1, name2, m2, m, userInput){
 }
 
 function init (nameSwitch) {
-   var m2 = document.getElementById("message-container");
-   var m = m2.elements["name"].value
+   var user, type;
+   var messageType = {
+   out: 'out-message',
+   in: 'in-message'};
+
+   m2 = document.getElementById("message-container");
+   m = m2.elements["name"].value
    name1 = "Giuseppe";
    name2 = "Jimmy John";
 
@@ -18,11 +23,13 @@ function init (nameSwitch) {
 
      case "send-button":
      userInput = name1 + ": " + m;
+     type = messageType.out;
      document.getElementById("message-container").reset();
      break;
 
      case "reply-button":
      userInput = name2 + ": " + m;
+     type = messageType.in;
      document.getElementById("message-container").reset();
      break;
 
@@ -32,8 +39,14 @@ function init (nameSwitch) {
 
    }
 
-   messages.push('-' + userInput + "<br>");
-
+   if (type === 'out-message')
+   {
+   messages.push('<font color = steelblue>' + '-' + userInput + "<br>");
    document.getElementById('displayMessage').innerHTML = messages.join('');
-
+   }
+   else if(type === 'in-message')
+   {
+   messages.push('<font color = salmon>' + '-' + userInput + "<br>");
+   document.getElementById('displayMessage').innerHTML = messages.join('');
+   }
 }
